620a115b08184e3d8a422bb35ac93fae98e3e428
Detravious J. Brinkley <d.brinkley97@gmail.com>
2022-01-13 23:31:14 -0800

    user interface set up
    + image view
    + 2 buttons
       1) Select an Image
       2) Capture an Image
    + 2 outlets
       1) imageView
       2) resultLabel
    + 2 actions

--- DominantObjectDetector/ViewController.swift
+++ DominantObjectDetector/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//    outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func onSelectImageFromPhotoLibrary(_ sender: Any) {
+    }
 
-
+    @IBAction func onSelectImageFromCamera(_ sender: Any) {
+    }
 }
 
